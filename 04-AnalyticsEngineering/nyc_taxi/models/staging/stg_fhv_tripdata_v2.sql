a9816ad1c284b9eec1292a4665db118aa313daed
gudpn.gin@gmail.com <gudpn.gin@gmail.com>
2024-02-24 23:45:36 +0000

    - documentation - fix timestamp -add facts

---
+++ 04-AnalyticsEngineering/nyc_taxi/models/staging/stg_fhv_tripdata_v2.sql
@@ -0,0 +1,32 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'fhv_tripdata_v2') }}
+
+),
+
+renamed as (
+
+    select
+        dispatching_base_num,
+        TIMESTAMP_MICROS(CAST(pickup_datetime /1000 as INT64)) as pickup_datetime ,
+        TIMESTAMP_MICROS(CAST(dropoff_datetime /1000 as INT64)) as dropoff_datetime ,
+        {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
+        {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    
+        {{ dbt.safe_cast("sr_flag", api.Column.translate_type("integer")) }} as sr_flag,
+
+        affiliated_base_number
+
+    from source
+
+)
+
+select * from renamed
+-- dbt build --select <model.sql> --vars '{is_test_run: false}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
